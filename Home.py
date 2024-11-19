c15d18b2149abf2f5acb9431ede70256b36d448f
GaddamAdvitha <119158848+GaddamAdvitha@users.noreply.github.com>
2024-11-20 00:14:59 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,14 +14,4 @@ This application offers interactive visualizations and machine learning models f
 Explore detailed insights by navigating to respective pages using the sidebar.
 """)
 
-# Provide Dataset Download Option
-st.header("Download Dataset")
-with open("merged_main_df.csv", "rb") as file:
-    st.download_button("Download Dataset", file, "merged_main_df.csv")
 
-# Provide Project Files Download as a ZIP
-st.header("Download Project Files")
-if st.button("Download Complete Project"):
-    zip_path = "project.zip"
-    with open(zip_path, "rb") as zf:
-        st.download_button("Download Project ZIP", zf, "project.zip")
